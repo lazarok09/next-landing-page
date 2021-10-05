76520532c56f6fa949bdc35bbe8456c00654fe2b
lazarok09 <lazarok09@gmail.com>
2021-10-05 17:18:43 -0300

    refactor:<MenuLink> migra o código para typescript

--- src/components/MenuLink/stories.tsx
+++ src/components/MenuLink/stories.tsx
@@ -1,4 +1,5 @@
-import { MenuLink } from '.';
+import { MenuLink, MenuLinkProps } from '.';
+import { Meta, Story } from '@storybook/react/types-6-0';
 
 export default {
   title: 'MenuLink',
@@ -10,9 +11,9 @@ export default {
   argTypes: {
     children: { type: 'string' },
   },
-};
+} as Meta;
 
-export const Template = (args) => {
+export const Template: Story<MenuLinkProps> = (args) => {
   return (
     <div style={{ display: 'flex' }}>
       <MenuLink {...args} />
