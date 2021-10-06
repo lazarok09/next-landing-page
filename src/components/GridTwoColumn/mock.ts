c2cedfce99d835d7dd32960ed908f69b19550d8b
lazarok09 <lazarok09@gmail.com>
2021-10-05 21:01:52 -0300

    refactor:<GridContent> migra para typescript

--- src/components/GridTwoColumn/mock.ts
+++ src/components/GridTwoColumn/mock.ts
@@ -3,4 +3,4 @@ export default {
   title: 'Grid Two Columns',
   text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa perspiciatis obcaecati recusandae eum, harum veniam sequi quisquam beatae, qui voluptates rerum minima vel laborum adipisci! Corporis beatae sunt quisquam ipsum.`,
   srcImage: 'assets/images/javascript.svg',
-} as Pick<GridTwoColumnsProps, 'title' | 'text' | 'srcImage'>;
+} as GridTwoColumnsProps;
